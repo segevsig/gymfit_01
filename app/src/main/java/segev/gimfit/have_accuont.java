554709ce671efbcecf41559456576c6ff1353168
segevuni85 <segevuni85@gmail.com>
2018-05-03 19:33:55 +0300

    ללא עדכונים מיוחדים... סתם חפירה

--- app/src/main/java/segev/gimfit/have_accuont.java
+++ app/src/main/java/segev/gimfit/have_accuont.java
@@ -17,6 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class have_accuont extends AppCompatActivity {
     EditText Username;
@@ -67,12 +68,16 @@ public class have_accuont extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
+                    FirebaseDatabase.getInstance().getReference().child("trainee")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("device_token")
+                            .setValue(FirebaseInstanceId.getInstance().getToken());
                     DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Coach")
                             .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
                     ref.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
                         @Override
                         public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
                            if (dataSnapshot.getValue()!=null){
+
                                Intent intent=new Intent(have_accuont.this,dashboard_activity.class);
                                Toast.makeText(have_accuont.this,"User is Loged In pass to private page",Toast.LENGTH_LONG).show();
                                startActivity(intent);
--- app/src/main/java/segev/gimfit/list_event_for_traning.java
+++ app/src/main/java/segev/gimfit/list_event_for_traning.java
@@ -1,8 +1,7 @@
 package segev.gimfit;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 public class list_event_for_traning extends AppCompatActivity {
@@ -14,6 +13,7 @@ public class list_event_for_traning extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_event_for_traning);
        name_of_traning= getIntent().getStringExtra("name").toString();
+
         Toast.makeText(list_event_for_traning.this,name_of_traning,Toast.LENGTH_LONG).show();
 
 
